6a47d5cb17d65bc1ba5af59dca115d44fcb4b888
Alex Kharunzha <a.kharunzha@gmail.com>
2019-04-21 18:01:13 +0300

    implement simple psr-4 autoload

--- 2dshapes/bootstrap.php
+++ 2dshapes/bootstrap.php
@@ -1,10 +1,21 @@
 <?php declare(strict_types=1);
 
+// load function and classes
 require_once __DIR__ . "/functions.php";
 $classesDir = __DIR__ . '/Classes/';
-$classes = scandir($classesDir);
-foreach ($classes as $class) {
-    if (is_file($classesDir.$class)) {
-        require_once $classesDir.$class;
+
+spl_autoload_register(function ($class) use ($classesDir) {
+    $namespace = 'Shapes\\';
+    if (strpos($class, $namespace) === 0) {
+        $class = str_replace($namespace, '', $class);
+        require_once $classesDir . $class . '.php';
     }
-}
+});
+
+
+// redirect exceptions to custom error output
+set_exception_handler(
+    function (Throwable $exception) {
+        onError($exception->getMessage());
+    }
+);
